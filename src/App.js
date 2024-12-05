186d64c52abb8f94de6a4dcf49d33b04c8bc0ffc
shamsad-alam <alamshamsad687@gmail.com>
2024-12-05 13:28:13 +0530

    uncomment main pages in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-// App.js
-// import React, { Component } from 'react';
+import React from 'react';
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import './App.css';
-// import LandingPage from './Component/LandingPage';
-// import LoginPage from './Component/LoginPage';
-// import RegistrationPage from './Component/RegistrationPage'
-// import ProductPage from './Component/ProductPage';
-// import ProductDetailsPage from './Component/ProductDetailsPage';
+import './App.css';
+import LandingPage from './Component/LandingPage';
+import LoginPage from './Component/LoginPage';
+import RegistrationPage from './Component/RegistrationPage'
+import ProductPage from './Component/ProductPage';
+import ProductDetailsPage from './Component/ProductDetailsPage';
 
-import ApiDatafetch from './Component/ApiDataFetch';
 
 function App() {
   return (
     <div className="App">
-      {/* <LandingPage />
+      <LandingPage />
       <LoginPage />
       <RegistrationPage />
       <ProductPage />
-      <ProductDetailsPage /> */}
-
-      <ApiDatafetch/>
+      <ProductDetailsPage />
     </div>
 
 
